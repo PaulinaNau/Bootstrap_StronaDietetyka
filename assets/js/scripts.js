8910759daad347776e0ea4c107a5a2b4a354e78a
PaulinaNau <paulina.naurecka@gmail.com>
2023-05-06 21:41:35 +0200

    hamburger menu in progress

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+
+
+
+
+function hamburger () {
+    const open = document.getElementById("hamburger-open");
+    const close = document.getElementById("hamburger-close");
+   
+    open.classList.toggle("d-none");;
+    close.classList.toggle("d-block");;
+}
